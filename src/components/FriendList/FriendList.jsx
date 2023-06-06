97193407799d72e3206e3efd676142fc21ad082e
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-06-06 19:57:37 +0300

    update

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from './FriendListItem';
-import css from './FriendList.module.css';
 
 export function FriendList({ friends }) {
   return (
-    <ul className={css.friendList}>
+    <ul>
       {friends.map(({ avatar, name, isOnline, id }) => (
         <FriendListItem
           key={id}
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,12 +1,11 @@
 import PropTypes from 'prop-types';
-import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className={css.item}>
-      <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
-      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-      <p className={css.name}>{name}</p>
+    <li>
+      <span>{isOnline}</span>
+      <img src={avatar} alt="User avatar" width="48" />
+      <p>{name}</p>
     </li>
   );
 };
