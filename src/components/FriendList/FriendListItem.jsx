ce2b484c2350cacd5f3ad50590179fe40669e4de
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-06-06 19:51:48 +0300

    update

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,13 +1,12 @@
 import PropTypes from 'prop-types';
-import css from './css/FriendListItem.module.css';
-// import css from './FriendListItem.module.css';
+import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li>
-      <span>{isOnline}</span>
-      <img src={avatar} alt="User avatar" width="48" />
-      <p>{name}</p>
+    <li className={css.item}>
+      <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 };
