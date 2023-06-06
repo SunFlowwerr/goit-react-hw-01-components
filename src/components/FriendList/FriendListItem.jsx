3e41b6a7bbca89ae54e30454d3b4694af03db2d7
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-06-06 16:01:56 +0300

    update

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './FriendListItem.module.css';
+import css from './css/FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
