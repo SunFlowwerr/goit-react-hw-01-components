e23746c720489de43d735c6fb1e6d16023b0a285
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-06-07 13:30:43 +0300

    update

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,9 +2,44 @@ import PropTypes from 'prop-types';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li>
-      <span>{isOnline}</span>
-      <img src={avatar} alt="User avatar" width="48" />
+    <li
+      style={{
+        display: flex,
+        flexDirection: row,
+        alignItems: center,
+        justifyContent: start,
+        gap: 10,
+        padding: 15,
+        width: 200,
+        height: 60,
+        boxSizing: border - box,
+        borderRadius: 4,
+        backgroundColor: '#e0ffff',
+        color: '#2f4f4f',
+        fontSize: 14,
+      }}
+    >
+      <span
+        style={{
+          width: 9,
+          height: 9,
+          boxSizing: border - box,
+          borderRadius: '100%',
+        }}
+      >
+        {isOnline}
+      </span>
+      <img
+        style={{
+          padding: 5,
+          boxSizing: border - box,
+          borderRadius: '100%',
+          backgroundColor: '#ffffff',
+        }}
+        src={avatar}
+        alt="User avatar"
+        width="48"
+      />
       <p>{name}</p>
     </li>
   );
