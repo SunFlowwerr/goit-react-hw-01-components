300d2ac113c5bbc4766fa909ca4a504cb50b1c1e
SkliarovaAnastasiia <skliarova16@gmail.com>
2023-06-07 13:40:15 +0300

    update

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -17,6 +17,8 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
         backgroundColor: '#e0ffff',
         color: '#2f4f4f',
         fontSize: 14,
+        boxShadow:
+          '0px 3px 1px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.08), 0px 2px 2px rgba(0, 0, 0, 0.12)',
       }}
     >
       <span
